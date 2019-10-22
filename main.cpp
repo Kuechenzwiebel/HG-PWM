eded369f297b6a668d49c3f3a3be17382f59f0a0
Kuechenzwiebel <tobiaspflueger@t-online.com>
2019-10-22 16:33:36 +0100

    Fixed PWM frequency for Raspberry Pi 4B

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@ int main() {
   wiringPiSetup();
 
   PWM pwm(0);
-/*
-  while(true) {
-    for(float i = 0.0f; i <= 1.0f; i+= 0.01f)
-      pwm.pulse(500.0f, 16.0f, i);
 
-    for(float i = 1.0f; i >= 0.0f; i-= 0.01f)
-      pwm.pulse(500.0f, 16.0f, i);
+  while(true) {
+	pwm.pulse(70.0f, 100.0f);
+/*
+    for(float i = 0.0f; i <= 1.0f; i+= 0.005f)
+      pwm.pulse(500.0f, 50.0f, i);
+
+    for(float i = 1.0f; i >= 0.0f; i-= 0.005f)
+      pwm.pulse(500.0f, 50.0f, i);*/
   }
-*/
+
   pwm.pulse(500.0f, 2500.0f);
 
   return 0;
--- pwm.cpp
+++ pwm.cpp
@@ -14,11 +14,11 @@ PWM::~PWM() {
 }
 
 void PWM::pulse(float freq, float time, float duty) {
-  printf("%Lf\n", 0.0025 * pow((long double) (e), (long double)(2.1914f * freq / 100.0f)));
+//  printf("%Lf\n", 0.0025 * pow((long double) (e), (long double)(2.1914f * freq / 100.0f)));
   chrono::steady_clock::time_point endPoint = chrono::steady_clock::now() + chrono::milliseconds(int(round(time)));
 
-  unsigned int delayHigh = round((1000000.0f / freq) * duty);
-  unsigned int delayLow = round((1000000.0f / freq) * (1.0f - duty));
+  unsigned int delayHigh = round((1000000.0f / freq) * duty) - 80;
+  unsigned int delayLow = round((1000000.0f / freq) * (1.0f - duty)) - 80;
 
   while(endPoint > chrono::steady_clock::now()) {
     digitalWrite(pin, HIGH);
@@ -30,8 +30,8 @@ void PWM::pulse(float freq, float time, float duty) {
 
 void PWM::pulses(float freq, unsigned long pulses, float duty) {
 
-  unsigned int delayHigh = round((1000000.0f / freq) * duty);
-  unsigned int delayLow = round((1000000.0f / freq) * (1.0f - duty));
+  unsigned int delayHigh = round((1000000.0f / freq) * duty) - 80;
+  unsigned int delayLow = round((1000000.0f / freq) * (1.0f - duty)) - 80;
 
   for(unsigned long i = 0; i < pulses; i++) {
     digitalWrite(pin, HIGH);
